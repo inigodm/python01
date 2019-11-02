b7ac55886a871e7f366d41bfc60298c950e4c4df
Korlat <amaia.berrocal@gmail.com>
2019-11-02 21:00:52 +0100

    Ejercicio 1 del M00 que devuelve saludo aleatorio al introducir el nombre.

--- Ejercicios/Ej01.py
+++ Ejercicios/Ej01.py
@@ -1,1 +1,8 @@
-print("Hola, ", input("¿Cómo te llamas?: "))
+import random
+
+saludos = ["Hola, {}", "Buenas, {}", "{}, ¿Que tal estas?", "Zer moduz, {}?"]
+saludo = saludos[random.randint(0,3)]
+
+nombre = input("¿Cómo te llamas? ")
+
+print(saludo.format(nombre))
